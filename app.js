0cf0911baf1498f808792f0de14815e09f1b8aff
Robin Messenger <robin@6joes.com>
2018-12-07 18:24:08 -0400

    Created full RESTful API with ability to get, create, update, and delete resources.

--- app.js
+++ app.js
@@ -13,6 +13,134 @@ const app = express();
 app.use(express.static('static'));
 const parser = bodyParser.urlencoded({extended: false});
 
+app.put('/month/:month/:year/:planned_income', (request, response) => {
+    const month = parseInt(request.params.month);
+    const year = parseInt(request.params.year);
+    const planned_income = parseInt(request.params.planned_income);
+
+    if (month > 0 && month <= 12 && year > 0 && planned_income >= 0) {
+        db.query(`update MONTH set planned_income = ${planned_income} where month = ${month} and year = ${year}`, (error, results) => {
+            if (error) {
+                response.status(500);
+                response.send(error);
+            } else {
+                response.send("Success.");
+                console.log(`Updated month: ${month}, ${year}, ${planned_income}`);
+            }
+        });
+    } else {
+        response.status(400);
+        response.send("Invalid data.");
+    }
+});
+
+app.put('/category/:month/:year/:name/:planned_expenses', (request, response) => {
+    const month = parseInt(request.params.month);
+    const year = parseInt(request.params.year);
+    const planned_expenses = parseInt(request.params.planned_expenses);
+    const name = ("" + request.params.name).replace(/[^A-Za-z0-9 ]/g, "");
+
+    if (month > 0 && month <= 12 && year > 0 && name.length > 0 && planned_expenses >= 0) {
+        db.query(`update CATEGORY set name = "${name}", planned_expenses = ${planned_expenses} where month = ${month} and year = ${year}`, (error, results) => {
+            if (error) {
+                response.status(500);
+                response.send(error);
+            } else {
+                response.send("Success.");
+                console.log(`Updated category: ${month}, ${year}, ${name}, ${planned_expenses}`);
+            }
+        });
+    } else {
+        response.status(400);
+        response.send("Invalid data.");
+    }
+});
+
+app.put('/item/:id/:month/:year/:category/:cost/:day/:name', (request, response) => {
+    const id = parseInt(request.params.id);
+    const month = parseInt(request.params.month);
+    const year = parseInt(request.params.year);
+    const day = parseInt(request.params.day);
+    const cost = parseInt(request.params.cost);
+    const category = ("" + request.params.category).replace(/[^A-Za-z0-9 ]/g, "");
+    const name = ("" + request.params.name).replace(/[^A-Za-z0-9 ]/g, "");
+
+    if (id && month > 0 && month <= 12 && year > 0 && cost >= 0 && day > 0 && category.length > 0 && name.length > 0) {
+        db.query(`update ITEM set month = ${month}, year = ${year}, category_name = "${category}", cost = ${cost}, day = ${day}, name = "${name}" where id = ${id}`, (error, results) => {
+            if (error) {
+                response.status(500);
+                response.send(error);
+            } else {
+                response.send("Success.");
+                console.log(`Updated item: ${id}, ${month}, ${year}, ${category}, ${cost}, ${day}, ${name}`);
+            }
+        });
+    } else {
+        response.status(400);
+        response.send("Invalid data.");
+    }
+});
+
+app.del('/category/:month/:year/:name', (request, response) => {
+    const month = parseInt(request.params.month);
+    const year = parseInt(request.params.year);
+    const name = ("" + request.params.name).replace(/[^a-zA-Z0-9 ]/g, "");
+
+    if (month > 0 && month <= 12 && year > 0 && name.length > 0) {
+        db.query(`delete from CATEGORY where month = ${month} and year = ${year} and name = "${name}"`, (error, results) => {
+            if (error) {
+                response.status(500);
+                response.send(error);
+            } else {
+                response.send("Success.");
+                console.log(`Deleted category: ${month}, ${year}, ${name}`);
+            }
+        });
+    } else {
+        response.status(400);
+        response.send("Invalid data.");
+    }
+});
+
+app.del('/item/:id', (request, response) => {
+    const id = parseInt(request.params.id);
+
+    if (id) {
+        db.query(`delete from ITEM where id = ${id}`, (error, results) => {
+            if (error) {
+                response.status(500);
+                response.send(error);
+            } else {
+                response.send("Success.");
+                console.log(`Deleted item: ${id}`);
+            }
+        });
+    } else {
+        response.status(400);
+        response.send("Invalid data.");
+    }
+});
+
+app.del('/month/:month/:year', (request, response) => {
+    const month = parseInt(request.params.month);
+    const year = parseInt(request.params.year);
+
+    if (month > 0 && month <= 12 && year > 0) {
+        db.query(`delete from MONTH where month = ${month} and year = ${year}`, (error, results) => {
+            if (error) {
+                response.status(500);
+                response.send(error);
+            } else {
+                response.send("Success.");
+                console.log(`Deleted month: ${month}, ${year}`);
+            }
+        });
+    } else {
+        response.status(400);
+        response.send("Invalid data.");
+    }
+});
+
 app.post('/month/:month/:year/:planned_income', (request, response) => {
   const month = parseInt(request.params.month);
   const year = parseInt(request.params.year);
@@ -34,19 +162,20 @@ app.post('/month/:month/:year/:planned_income', (request, response) => {
   }
 });
 
-app.post('/category/:month/:year/:name', (request, response) => {
+app.post('/category/:month/:year/:name/:planned_expenses', (request, response) => {
   const month = parseInt(request.params.month);
   const year = parseInt(request.params.year);
+  const planned_expenses = parseInt(request.params.planned_expenses);
   const name = ("" + request.params.name).replace(/[^A-Za-z0-9 ]/g, "");
 
-  if (month > 0 && month <= 12 && year > 0 && name.length > 0) {
-    db.query(`insert into MONTH (month, year, planned_income) values (${month}, ${year}, ${planned_income})`, (error, results) => {
+  if (month > 0 && month <= 12 && year > 0 && name.length > 0 && planned_expenses >= 0) {
+    db.query(`insert into CATEGORY (month, year, name, planned_expenses) values (${month}, ${year}, "${name}", ${planned_expenses})`, (error, results) => {
       if (error) {
         response.status(500);
         response.send(error);
       } else {
         response.send("Success.");
-        console.log(`Added category: ${month}, ${year}, ${name}`);
+        console.log(`Added category: ${month}, ${year}, ${name}, ${planned_expenses}`);
       }
     });
   } else {
@@ -64,7 +193,7 @@ app.post('/item/:month/:year/:category/:cost/:day/:name', (request, response) =>
   const name = ("" + request.params.name).replace(/[^A-Za-z0-9 ]/g, "");
 
   if (month > 0 && month <= 12 && year > 0 && cost >= 0 && day > 0 && category.length > 0 && name.length > 0) {
-    db.query(`insert into MONTH (month, year, planned_income) values (${month}, ${year}, ${planned_income})`, (error, results) => {
+    db.query(`insert into ITEM (month, year, category_name, cost, day, name) values (${month}, ${year}, "${category}", ${cost}, ${day}, "${name}")`, (error, results) => {
       if (error) {
         response.status(500);
         response.send(error);
